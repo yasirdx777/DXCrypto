fe0f0e49f982f1a2ed993dcc7c0a524e6c24c3a6
Yasir Romaya <yasir.nabil@iq.email>
2023-01-13 23:56:59 +0300

    Initial Commit

---
+++ DXCrypto/DXCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DXCryptoApp.swift
+//  DXCrypto
+//
+//  Created by iq on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DXCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
